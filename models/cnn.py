b138f8d2b2f175f706ee50af99050d2b93ada87a
Gopinathmu <duraigopinath23@gmail.com>
2025-06-18 14:35:14 +0530

    Initial commit: Emotion Detection App with Streamlit

---
+++ models/cnn.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EmotionCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(EmotionCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.25)
+        self.fc1 = nn.Linear(128 * 6 * 6, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 48x48 -> 24x24
+        x = self.pool(F.relu(self.conv2(x)))  # 24x24 -> 12x12
+        x = self.pool(F.relu(self.conv3(x)))  # 12x12 -> 6x6
+        x = x.view(-1, 128 * 6 * 6)
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        return self.fc2(x)
